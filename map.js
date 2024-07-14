69d2326878eba2ff55efa4373d8724a6e8778ffa
whisper0606 <90876918+whisper0606@users.noreply.github.com>
2024-07-14 10:08:23 +1000

    Add files via upload

--- map.js
+++ map.js
@@ -1,13 +1,5 @@
 var map = L.map('map').setView([-27.4705,153.0260],12); // setup the map
 
-L.tileLayer('https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png', { // base layer, zoom, attribution
-    maxZoom: 17,
-    minZoom: 12,
-    attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
-}).addTo(map);
-
-//var debug = L.layerGroup([debugMarker]);
-
 var debugMarker = L.marker([-27.4584,153.0343]).addTo(map);
 debugMarker.bindPopup("This is where you are right now.")
 
@@ -19,3 +11,19 @@ var debugCircle = L.circle([-27.3943, 153.1178], {
 }).addTo(map);
 debugCircle.bindPopup("This is the airport.")
 
+var debugLayer = L.layerGroup([debugMarker, debugCircle]);
+
+var overlayMaps = {
+    "Debug": debugLayer
+};
+
+L.tileLayer('https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', { // base layer, zoom, attribution
+    maxZoom: 17,
+    minZoom: 12,
+    layers: [debugLayer],
+    attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attributions">CARTO</a>',
+}).addTo(map);
+
+
+
+var layerControl = L.control.layers(overlayMaps).addTo(map);
